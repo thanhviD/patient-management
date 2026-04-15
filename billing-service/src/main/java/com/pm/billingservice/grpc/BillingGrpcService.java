2b8bf61960452e048e296b105e5af31e9ee3d5b6
thanhviD <datvithanh123@gmail.com>
2026-04-15 18:12:50 +0700

    update billing properties file

---
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,28 @@
+package com.pm.billingservice.grpc;
+
+import billing.BillingResponse;
+import billing.BillingServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+
+    @Override
+    public void createBillingAccount(billing.BillingRequest billingRequest,
+                                      StreamObserver<BillingResponse> responseObserver) {
+        log.info("CreateBillingAccount request receive {}", billingRequest.toString());
+
+        //Business logic - e.g: save to database, perform calculation etc.
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId("12345")
+                .setStatus("ACTIVE")
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
