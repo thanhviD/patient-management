cbeaf9dd5c57d783d64ed7ac57c2273e87f5d9c0
thanhviD <datvithanh123@gmail.com>
2026-07-07 11:00:41 +0700

    add kafka patient service producer test 36

--- patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
+++ patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
@@ -1,16 +1,41 @@
 package com.pm.patientservice.kafka;
 
+import com.pm.patientservice.model.Patient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class KafkaProducer {
 
+    private static final Logger log = LoggerFactory.getLogger(KafkaProducer.class);
     private final KafkaTemplate<String, byte[]> kafkaTemplate;
 
 
     public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendEvent(Patient patient) {
+        PatientEvent event = PatientEvent.newBuilder()
+                .setPatientId(patient.getId().toString())
+                .setName(patient.getName())
+                .setEmail(patient.getEmail())
+                .setEventType("PATIENT CREATED")
+                .build();
+
+        try {
+            kafkaTemplate.send("patient", event.toByteArray());
+//            kafkaTemplate.send("patient", event.toByteArray())
+//                    .get(10, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            log.error("Error sending PatientCreated event: {}", event);
+        }
+
     }
 }
--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -5,6 +5,7 @@ import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.exception.EmailAlreadyExistsException;
 import com.pm.patientservice.exception.PatientNotFoundException;
 import com.pm.patientservice.grpc.BillingServiceGrpcClient;
+import com.pm.patientservice.kafka.KafkaProducer;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
@@ -19,10 +20,12 @@ import java.util.UUID;
 public class PatientService {
     private final PatientRepository patientRepository;
     private final BillingServiceGrpcClient billingServiceGrpcClient;
+    private final KafkaProducer kafkaProducer;
 
-    public PatientService(PatientRepository patientRepository, BillingServiceGrpcClient billingServiceGrpcClient) {
+    public PatientService(PatientRepository patientRepository, BillingServiceGrpcClient billingServiceGrpcClient, KafkaProducer kafkaProducer) {
         this.patientRepository = patientRepository;
         this.billingServiceGrpcClient = billingServiceGrpcClient;
+        this.kafkaProducer = kafkaProducer;
     }
 
     public List<PatientResponseDTO> getPatient() {
@@ -45,6 +48,10 @@ public class PatientService {
         billingServiceGrpcClient.createBillingAccount(newPatient.getId().toString(),
                 newPatient.getName(),
                 newPatient.getEmail());
+
+        // send event to kafka producer
+        kafkaProducer.sendEvent(newPatient);
+
         return PatientMapper.toDTO(newPatient);
     }
 
